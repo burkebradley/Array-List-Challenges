b7f182d73f6eadeb6f6d21e32e44038717fe6bf3
burke.bradley <burke.bradley@MU-C1-04.mullen.pvt.k12.co.us>
2018-09-20 14:19:04 -0600

    initial commit

---
+++ ArrayListChallenges/src/ArrayListChallenges.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class ArrayListChallenges
+	{
+
+		public static void main(String[] args)
+			{
+				ArrayList<Player> team = new ArrayList<Player>();
+				team.add(new Player(10));
+				team.add(new Player(9));
+				team.add(new Player(11));
+				team.add(new Player(23));
+				team.add(new Player(0));
+				double sum = 0;
+			
+			}
+
+	}
