4e7cdd5477d4a05f73ae1b9fa899d1e2eb64f875
burke.bradley <burke.bradley@MU-C1-04.mullen.pvt.k12.co.us>
2018-09-21 15:01:34 -0600

    finish

--- ArrayListChallenges/src/ArrayListChallenges.java
+++ ArrayListChallenges/src/ArrayListChallenges.java
@@ -4,13 +4,17 @@ public class ArrayListChallenges
 
 		public static void main(String[] args)
 			{
-				ArrayList<Player> team = new ArrayList<Player>();
-				team.add(new Player(10));
-				team.add(new Player(9));
-				team.add(new Player(11));
-				team.add(new Player(23));
-				team.add(new Player(0));
+				ArrayList<Double>team = new ArrayList<Double>();
+				team.add(10.0);
+				team.add(9.5);
+				team.add(11.3);
+				team.add(23.4);
+				team.add(0.3);
 				double sum = 0;
+				for (int i = 0; i<Double.size(); i++)
+					{
+						System.out.println(Double[i]);
+					}
 			
 			}
 
